ffc076e81ac8af36b9df5a7422ae0916cec95bc9
towerspreston-create <towers.preston@gmail.com>
2026-01-15 17:22:12 -0500

    Delete prompts directory

--- prompts/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Prompts module for Fires Coordinator Agent."""
-from .system_prompt import SYSTEM_PROMPT, get_system_prompt_with_context
-
-__all__ = ["SYSTEM_PROMPT", "get_system_prompt_with_context"]
